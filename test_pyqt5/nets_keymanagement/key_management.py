872edd53c20032910534ed69f8ee6f324bb9d59f
zealotnt <tranminhtam.10192@gmail.com>
2017-02-27 14:57:03 +0800

    support sign firmware

--- test_pyqt5/nets_keymanagement/key_management.py
+++ test_pyqt5/nets_keymanagement/key_management.py
@@ -29,6 +29,89 @@ import mainwindow_gui_auto
 CURRENT_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + '/'
 WINDOWS_TITLE = "STYL Key Management"
 
+
+RAW_KEY_FILE			= CURRENT_DIR + "firmware_sign_key.raw"
+CASIGN_INI_FILE			= CURRENT_DIR + "ca_sign_build.ini"
+SESSION_BUILD_INI_FILE 	= CURRENT_DIR + "session_build.ini"
+SB_SCRIPT_FILE 			= CURRENT_DIR + "sb_script.txt"
+BINARY_S19_FILE_NAME	= "binary.s19"
+BINARY_S19_FILE 		= CURRENT_DIR + BINARY_S19_FILE_NAME
+SB_SCRIPT_CONTENT 		= "write-file %s" % (BINARY_S19_FILE_NAME)
+SCP_OUT_DIR				= CURRENT_DIR + "scp_out"
+
+def genS19File(binPath):
+	command = 'objcopy -I binary -O srec --srec-forceS3 --srec-len=128 --adjust-vma 0x10000000 %s %s' % (binPath, BINARY_S19_FILE)
+	os.system(command)
+
+def signFirmware(keyPath, firmwarePath, firmwareType):
+	casign_ini_file = {
+		"algo": "ecdsa",
+		"ecdsa_file": "Casign/crk_ecdsa_angela_test.key",
+		"ca": "Casign/suribootloader.bin",
+		"sca": "Casign/suribootloader.signed.bin",
+		"load_address": "10000000",
+		"jump_address": "10000020",
+		"arguments": "",
+		"application_version": "010A0B03",
+		"verbose": "yes"
+	}
+	session_ini_file = {
+		"session_mode": "SCP_ANGELA_ECDSA",
+		"verbose": "no",
+		"output_file": "%s/out" % (SCP_OUT_DIR),
+		"pp": "ECDSA",
+		"addr_offset": "00000000",
+		"chunk_size": "4094",
+		"script_file": "sb_script.txt",
+		"ecdsa_file": "session_build/maximtestcrk.key"
+	}
+	# Check the validity of private key
+
+	# Generate the raw value of private key
+
+	# Write the "ca_sign_build.ini" file
+	with open(CASIGN_INI_FILE, "w") as f:
+		for item in casign_ini_file:
+			print(item, casign_ini_file[item])
+			f.write("%s=%s\n" % (item, casign_ini_file[item]))
+		f.close()
+
+	# Sign the firmware with Casign
+	if firmwareType == "SURIBL":
+		# Sign with full param
+		os.system('./Casign/ca_sign_build.exe')
+		pass
+	elif firmwareType == "SURISDK":
+		# Sign without the 32bytes header
+		return
+	else:
+		msgBoxError("Error", "Unregcognize firmware type: %s" % firmwareType)
+		return
+
+	# Generate the SCP packets if the firmware is bootloader
+	if firmwareType == "SURIBL":
+		with open(SB_SCRIPT_FILE, "w") as f:
+			f.write("%s\n" % SB_SCRIPT_CONTENT)
+			f.close()
+
+		with open(SESSION_BUILD_INI_FILE, "w") as f:
+			for item in session_ini_file:
+				print(item, session_ini_file[item])
+				f.write("%s=%s\n" % (item, session_ini_file[item]))
+			f.close()
+
+		genS19File(firmwarePath)
+
+		if not os.path.exists(SCP_OUT_DIR):
+			os.makedirs(SCP_OUT_DIR)
+
+		os.system('./session_build/session_build.exe')
+		return
+
+	# Generate the sign firmware if the firmware is surisdk
+
+	return
+
 def generateECKey(keyname, keypass=""):
 	private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
 
@@ -126,7 +209,7 @@ class MainGui(mainwindow_gui_auto.Ui_MainWindow):
 		self.initHandler()
 
 	def initHandler(self):
-		self.btnSignFirmware.clicked.connect(self.signFirmware)
+		self.btnSignFirmware.clicked.connect(self.signFirmwareHandler)
 		self.btnSignChooseFirmwarePath.clicked.connect(self.showFileDialog)
 		self.btnSignChoosePrivateKeyPath.clicked.connect(self.showFileDialog)
 		self.btnGenKeys.clicked.connect(self.generateKey)
@@ -154,7 +237,7 @@ class MainGui(mainwindow_gui_auto.Ui_MainWindow):
 		msgBoxInfo("Success", 'Private key "%s" successfully generated' % keyname)
 		return
 
-	def signFirmware(self):
+	def signFirmwareHandler(self):
 		# validate the input
 		if not(os.path.isfile(self.editSignKeyPath.text())):
 			msgBoxError("Error", 'Private key "%s" not found' % self.editSignKeyPath.text())
@@ -165,6 +248,7 @@ class MainGui(mainwindow_gui_auto.Ui_MainWindow):
 			return
 
 		# sign the firmware
+		signFirmware(self.editSignKeyPath.text(), self.editFirmwarePath.text(), "SURIBL")
 
 		# response to UI
 		msgBoxInfo("Success", 'Firmware "%s" successfully signed' % self.editFirmwarePath.text())
--- test_pyqt5/nets_keymanagement/note.md
+++ test_pyqt5/nets_keymanagement/note.md
@@ -7,5 +7,14 @@ pyuic5 -x <ui-file> -o <py-file>
 pyuic5 -x mainwindow.ui -o mainwindow_gui_auto.py
 ```
 
+# Generate the link folder to secureROM's folder
+```bash
+# Link to Casign folder
+ln -s /home/zealot/workspace_sirius/secureROM/Host/casign/bin/linux Casign
+
+# Link to session_build folder
+ln -s /home/zealot/workspace_sirius/secureROM/Host/session_build/bin/linux session_build
+```
+
 # Ref
 [Ref-1](http://projects.skylogic.ca/blog/how-to-install-pyqt5-and-build-your-first-gui-in-python-3-4/)
