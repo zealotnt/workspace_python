bdf47d4dcf5253b51de46470c9b05289c2a37650
zealotnt <tranminhtam.10192@gmail.com>
2017-12-08 09:57:02 +0700

    get filename after extracting

--- test_expect/test_baseline.py
+++ test_expect/test_baseline.py
@@ -1,3 +1,4 @@
+import subprocess
 from pexpect import pxssh
 import pexpect, getpass
 import sys
@@ -10,17 +11,62 @@ import shutil
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 
+class bcolors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	OKYEL = '\033[33m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+
 def CopyBaseline(targetFolder):
 	SetNetworkInterface(HOST_INTERFACE, HOST_IP, HOST_PASSWORD)
 	ScpDownloadFrom("/home/root/fw_backup/baseline", "root", BOARD_PASSWORD, BOARD_IP, targetFolder)
 
+def GotFirmware(obj):
+	print (" ... Got %s%s%s" % (bcolors.OKGREEN, obj, bcolors.ENDC))
+
+def Extractfile(file_path, from_folder, to_folder):
+	tarExamineCmd = "bsdtar -tf %s/%s" % (from_folder, file_path)
+	pListFile = subprocess.Popen(['bsdtar', '-tf', from_folder+"/"+file_path],
+		stdin=subprocess.PIPE,
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE)
+	output, err = pListFile.communicate()
+	rc = pListFile.returncode
+
+	tarCmd = "bsdtar -xzf %s/%s -C %s" % (from_folder, file_path, to_folder)
+	rc = os.system(tarCmd)
+	if rc != 0:
+		print_err("err when extracting file " + file_path)
+		return None
+	if output.count("\n") <= 1:
+		# only 1 firmware, strip the new line
+		output = output.strip('\n')
+		output = output.strip('\r')
+		pass
+	else:
+		# multiple firmware in a compressed file
+		output = output.replace("\r\n", ",")
+		output = output.replace("\n\r", ",")
+		output = output.replace("\r", ",")
+		output = output.replace("\n", ",")
+	return output
+
 def ExtractAll(folder):
 	for fn in os.listdir(folder):
 		if fn.endswith(".tar.xz"):
-			print ("=> Extracting: %s/%s" % (folder, fn))
-			ret = os.system("bsdtar -xzf %s/%s -C %s" % (folder, fn, folder))
-			if ret != 0:
-				print ("Error when extracting %s/%s" % (folder, fn))
+			sys.stdout.write ("=> Extracting: %s/%-30.30s to %s" % (folder, fn, folder))
+			extractedFile = Extractfile(fn, folder, folder)
+			GotFirmware(extractedFile)
+			if os.path.basename(extractedFile) != extractedFile:
+				path_from = folder+"/"+extractedFile
+				path_to = folder+"/"+os.path.basename(extractedFile)
+				print ("\t move %s to %s" % (path_from, path_to))
+				shutil.move(path_from, path_to)
 
 def DoHash(file_path):
 	with open(file_path, "rb") as f:
@@ -76,8 +122,9 @@ if __name__ == '__main__':
 		ExtractAll('./BASELINE_TARGET')
 
 		# extract fwPath to BASELINE_TEMP
-		print ("=> Extracting %s to BASELINE_TEMP" % (options.fwPath))
-		os.system("bsdtar -xzvf %s -C ./BASELINE_TEMP" % (options.fwPath))
+		sys.stdout.write ("=> Extracting %s to BASELINE_TEMP" % (options.fwPath))
+		extractedFile = Extractfile(options.fwPath, "./", "./BASELINE_TEMP")
+		GotFirmware(extractedFile)
 
 		# compare all json in BASELINE_TARGET with BASELINE_TEMP
 		for fn in os.listdir('./BASELINE_TARGET'):
@@ -87,12 +134,12 @@ if __name__ == '__main__':
 				if os.path.isfile(path_temp):
 					sys.stdout.write ("=> Comparing %-40.40s with   %-40.40s" % (path_target, path_temp))
 					if DoHash(path_target) == DoHash(path_temp):
-						print (" [OK]")
+						print (" [%sOK%s]" % (bcolors.OKGREEN, bcolors.ENDC))
 						passCount += 1
 					else:
-						print (" [FAIL]")
+						print (" [%sFAIL%s]" % (bcolors.FAIL, bcolors.ENDC))
 				else:
-					print ("Missing %s" % (path_temp))
+					print ("=> Missing %s" % (path_temp))
 
 		if passCount >= NUM_OF_FIRMWARE:
 			# if pass, ask for replace BASELINE_TEMP with current BASELINE
@@ -101,4 +148,4 @@ if __name__ == '__main__':
 				shutil.copytree("./BASELINE_TEMP", "./BASELINE")
 		else:
 			# if fail prompt error and exit
-			print ("Compare firmware fail !!!")
+			print ("Compare firmware fail (pass %d/%d)!!!" % (passCount, NUM_OF_FIRMWARE))
