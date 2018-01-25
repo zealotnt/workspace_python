326678557d0e4845b77e642fdc299574b577f643
zealotnt <tranminhtam.10192@gmail.com>
2018-01-25 16:00:03 +0700

    add upload functionality for flask-file-server

--- test_websever/test_flask_serve_file.py
+++ test_websever/test_flask_serve_file.py
@@ -1,13 +1,73 @@
-from flask import Flask, request, send_from_directory
+# Upload example at [Ref](http://flask.pocoo.org/docs/0.12/patterns/fileuploads/)
+# Download example at [Ref]()
+
+from flask import Flask, request, send_from_directory, redirect, url_for
+from werkzeug.utils import secure_filename
+from werkzeug import SharedDataMiddleware
+import os
+import inspect
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+UPLOAD_FOLDER = CURRENT_DIR + os.sep + 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
+
+def ensure_dir(file_path):
+	directory = os.path.dirname(file_path)
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+
+def allowed_file(filename):
+	return True
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit a empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('uploaded_file',
+									filename=filename))
+	return '''
+	<!doctype html>
+	<title>Upload new File</title>
+	<h1>Upload new File</h1>
+	<form method=post enctype=multipart/form-data>
+	  <p><input type=file name=file>
+		 <input type=submit value=Upload>
+	</form>
+	'''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 @app.route('/download/<path:path>')
 def send_js(path):
-    return send_from_directory('download', path)
+	return send_from_directory('download', path)
 
 if __name__ == "__main__":
+	ensure_dir(UPLOAD_FOLDER)
 	app.run(
 		debug=True,
 		port=8000,
