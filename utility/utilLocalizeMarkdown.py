7f3c00ff5115eb0244d7468f7423cf6652fa0143
Tran Tam <tranminhtam.10192@gmail.com>
2018-01-31 01:39:54 +0700

    add util to localize markdown file
    + it will search all images in markdown file
    and download to local in pics folder, and change
    the content in markdown file to use the local images

---
+++ utility/utilLocalizeMarkdown.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: zealotnt
+# @Date:   2018-01-31 00:21:20
+#
+# utilLocalizeMarkdown.py
+
+
+# ---- IMPORTS
+import os
+import sys
+import re
+import shutil
+import imghdr
+import textwrap
+from optparse import OptionParser, OptionGroup
+import colorama
+import urllib, urlparse
+# https://stackoverflow.com/questions/27981545/suppress-insecurerequestwarning-unverified-https-request-is-being-made-in-pytho
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# ---- CONSTANTS
+
+# ---- GLOBALS
+def download_file(url, download_file_name):
+	# https://stackoverflow.com/questions/33488179/how-do-i-download-pdf-file-over-https-with-python
+	# Better comparison: https://stackoverflow.com/a/45279314
+	import requests
+	r = requests.get(url, verify=False, stream=True)
+	r.raw.decode_content = True
+	with open(download_file_name, 'wb') as f:
+		shutil.copyfileobj(r.raw, f)
+
+def print_err(text):
+	print (colorama.Fore.RED + "Error: " + text + colorama.Style.RESET_ALL)
+
+def print_noti(text):
+	print (colorama.Fore.YELLOW + text + colorama.Style.RESET_ALL)
+
+def is_external_link(link):
+	return True
+
+def rchop(thestring, ending):
+	# https://stackoverflow.com/questions/3663450/python-remove-substring-only-at-the-end-of-string
+	if thestring.endswith(ending):
+		return thestring[:-len(ending)]
+	return thestring
+
+def start_meld(original_file, new_file):
+	os.system("meld %s %s" % (original_file, new_file))
+
+def yes_or_no(question):
+	reply = str(raw_input(question+' (y/n): ')).lower().strip()
+	if reply[0] == 'y':
+		return True
+	if reply[0] == 'n':
+		return False
+	else:
+		return yes_or_no("Uhhhh... please enter ")
+
+def main():
+	EPILOG = textwrap.dedent(
+	"""
+	This script will search all images in markdown file and download to local in pics folder,
+	and change the content in markdown file to use the local images.
+	""")
+	parser = OptionParser(epilog=EPILOG)
+	parser.add_option(  "-f",
+						dest="file_path",
+						type="string",
+						help="path to markdown file to be localized")
+	(options, args) = parser.parse_args()
+
+	if options.file_path is None:
+		parser.print_help()
+		print_err("markdown file path is required")
+		sys.exit(-1)
+
+	colorama.init()
+	markdown_file_path = options.file_path
+	markdown_file_name_full = os.path.basename(markdown_file_path)
+	markdown_file_name = os.path.splitext(markdown_file_name_full)[0]
+	if not os.path.isfile(markdown_file_path):
+		print_err("%s does not exist" % markdown_file_path)
+		sys.exit(-1)
+
+	with open(markdown_file_path, "r") as f:
+		content = []
+		for line in f:
+			content.append(line)
+
+		pics_count = 0
+		# Create the pics folder if not created
+		if not os.path.exists("pics"):
+			os.makedirs("pics")
+
+		# Loop through line by line
+		for idx, line in enumerate(content):
+			# Find line that contain the image link
+			image_line_patters = [r"!\[(.*)\]\((.+)\)"]
+			for pattern in image_line_patters:
+				match = re.findall(pattern, line, re.MULTILINE)
+				# There should not more than 2 image in one line
+				if len(match) == 1:
+					image_url = match[0][1]
+					# check if the image is from the internet
+					if is_external_link(image_url) == False:
+						print_noti("Image %s is not from internet, ignore" % image_url)
+						continue
+
+					print_noti("We get image: %s" % image_url)
+
+					# download the image
+					# https://stackoverflow.com/questions/3663450/python-remove-substring-only-at-the-end-of-string
+					image_url = rchop(image_url, "/")
+					# https://stackoverflow.com/questions/18727347/how-to-extract-a-filename-from-a-url-append-a-word-to-it
+					to_download = os.path.basename(urlparse.urlparse(image_url).path)
+					download_file(image_url, to_download)
+					# TODO: should check ok
+
+					# extract extension from file-name seems not works, because some file has weird ending text
+					# extension = os.path.splitext(to_download)[1]
+					# => so we calculate image type by using python's imghdr
+					# https://docs.python.org/2/library/imghdr.html
+					extension = imghdr.what(to_download)
+
+					# put the downloaded image with file name pattern: pics/<md-file-name>-<pic-count>
+					new_location = "pics/%s-%d.%s" % (markdown_file_name, pics_count, extension)
+					print_noti("\t=> Save to %s" % new_location)
+					shutil.move(to_download, new_location)
+					pics_count += 1
+
+					content[idx] = content[idx].replace(image_url, new_location)
+
+		# Create new markdown file, with file name: <md-file-name>.new.md
+		markdown_new_file = markdown_file_name + ".new.md"
+		new_md_file = open(markdown_new_file, 'w')
+		for item in content:
+			new_md_file.write("%s" % item)
+		new_md_file.close()
+
+		if yes_or_no("Do you want to compare org_file: %s and new_file: %s ?" % (markdown_file_path, markdown_new_file)) == True:
+			start_meld(markdown_file_path, markdown_new_file)
+
+# ---- MAIN
+if __name__ == "__main__":
+	main()
